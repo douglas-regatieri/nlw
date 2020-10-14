cb6bdaf8f55ae7681238eb7c0e9e0d955e1d67b3
Douglas Regatieri <douglasregatieri2009@gmail.com>
2020-10-14 11:31:01 -0300

    Implementação do Mapa na pag. Ofanatos

---
+++ public/scripts/page-orphanages.js
@@ -0,0 +1,22 @@
+"use strict";
+const map = L.map("mapid").setView([-20.5394322, -47.4082936], 15);
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
+
+const icon = L.icon({
+  iconUrl: './public/images/map-marker.svg',
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [170, 2]
+})
+
+const popup = L.popup({
+  closeButton: false,
+  className: 'map-popup',
+  minWidth: 240,
+  minHeigth: 240  
+}).setContent('Lar das Meninas <a href="orphanage.html?id=1" class="choose-orphanage"><img src="./public/images/arrow-white.svg"/></a>')
+
+L.marker([-20.5394322, -47.4082936],{ icon })
+  .addTo(map)
+  .bindPopup(popup);
